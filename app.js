55d613f86680f037552a823f46bc1802dda1a9fa
heyitshurberto <hubkiree@outlook.com>
2026-05-12 14:02:44 +0700

    Fix pushToGitHub: stop auto-pushing runtime log files

--- app.js
+++ app.js
@@ -3693,7 +3693,7 @@ const renderLoginPage = () => `
 
     @media (min-width: 769px) and (max-width: 1024px) {
       body {
-        zoom: 120%;
+        zoom: 150%;
       }
     }
     body::after {
